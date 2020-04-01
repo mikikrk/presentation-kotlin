32d2538e44e1bb81f299b352a90c0b7f82ca80c3
mikikrk <mnowak@worldremit.com>
2020-04-01 13:51:21 +0200

    Cleaned up setup for startup

--- src/main/java/com/nowak/mikolaj/JavaSandbox.java
+++ src/main/java/com/nowak/mikolaj/JavaSandbox.java
@@ -1,13 +1,6 @@
 package com.nowak.mikolaj;
 
-import com.nowak.mikolaj.step_1_syntax.Bar;
-
 public class JavaSandbox {
-
-    public Bar bar = new Bar(
-            1,
-            "Jon"
-    );
 
     public void playWithKotlin() {
     }
