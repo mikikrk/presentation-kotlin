e4fb116f52ec8abc3fdb0c6417bc91761a52bf4d
mikikrk <mnowak@worldremit.com>
2020-04-05 18:01:07 +0200

    Created Generic Extends and Super Java example

---
+++ src/main/java/com/nowak/mikolaj/generics/ExtendsSuper.java
@@ -0,0 +1,62 @@
+package com.nowak.mikolaj.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExtendsSuper {
+
+    interface Super {
+        Super superMethod();
+    }
+
+    interface Child extends Super {
+        Child childMethod();
+    }
+
+    interface ChildOfChild extends Child {
+        ChildOfChild childOfChildMethod();
+    }
+
+
+
+
+    Child getExtends(List<? extends Child> list, int index) {
+        return list.get(index).childMethod();
+    }
+
+    Child getSupers(List<? super Child> list, int item) {
+        return list.get(item).childMethod();
+    }
+    
+    void getting() {
+        getExtends(new ArrayList<Super>(), 0);
+        getExtends(new ArrayList<Child>(), 0);
+        getExtends(new ArrayList<ChildOfChild>(), 0);
+        
+        getSupers(new ArrayList<Super>(), 0);
+        getSupers(new ArrayList<Child>(), 0);
+        getSupers(new ArrayList<ChildOfChild>(), 0);
+    }
+
+
+
+
+    void appendExtends(List<? extends Child> list, Child item){
+        list.add(item);
+    }
+    void appendSupers (List<? super Child> list, Child item){
+        list.add(item);
+    }
+
+    private Child child;
+
+    void appending() {
+        appendExtends(new ArrayList<Super>(), child);
+        appendExtends(new ArrayList<Child>(), child);
+        appendExtends(new ArrayList<ChildOfChild>(), child);
+
+        appendSupers(new ArrayList<Super>(), child);
+        appendSupers(new ArrayList<Child>(), child);
+        appendSupers(new ArrayList<ChildOfChild>(), child);
+    }
+}
