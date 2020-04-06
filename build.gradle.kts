0f492ab292800d933347a8cc204b1e89126a898a
mikikrk <mnowak@worldremit.com>
2020-04-06 22:17:06 +0200

    Added coroutines dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime:1.3.41")
 }
